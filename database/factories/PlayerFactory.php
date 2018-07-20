017b727d72d0a0721aabd0ae9f14091e6efaf3a9
Methir <methir.game@gmail.com>
2018-07-20 16:13:28 -0300

    Criada a factory player

---
+++ database/factories/PlayerFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Player::class, function (Faker $faker) {
+    return [
+        'credential' => 0,
+        'gold' => 0,
+        'cash' => 0,
+        'detail' => $faker->text,
+        'image' => 'default.jpg',
+    ];
+});
